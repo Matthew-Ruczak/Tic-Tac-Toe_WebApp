5412e2f09c38b27fa0d8e28a2391f3b611594bfa
Matthew Ruczak <MatthewRuczak@gmail.com>
2021-04-23 00:07:18 -0500

    Add variables & properies in TicTacToeGame Class

--- js/ticTacToeGame.js
+++ js/ticTacToeGame.js
@@ -1,1 +1,16 @@
-class TicTacToeGame{}
+const playersTurnElement = $("#playersTurn"); //Holding reference to the GUI text to show who's turn it is
+const playerOneScoreElement = $('#playerOneScore'); //Holding reference to the GUI text for player 1's score
+const playerTwoScoreElement = $('#playerTwoScore'); //Holding reference to the GUI text for player 2's score
+const notificationOuterBox = $('#notificationOuterBox');
+const notificationMessage = $('#notificationMessage');
+const ticTacToeSquareElements = $(".ticTacToeSquares"); //Getting reference to all tic tac toe squares
+
+class TicTacToeGame{
+    playerOneScore;
+    playerTwoScore;
+
+    constructor(){
+        playerOneScore = 0;
+        playerTwoScore = 0;
+    }
+}
