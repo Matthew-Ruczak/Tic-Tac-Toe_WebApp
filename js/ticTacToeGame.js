0145cedeba460dd42dfad6e94759e3a997258ae3
Matthew Ruczak <MatthewRuczak@gmail.com>
2021-04-23 00:12:47 -0500

    Add a property to hold reference to the curr Tic Tac Toe Round Object.

--- js/ticTacToeGame.js
+++ js/ticTacToeGame.js
@@ -14,9 +14,15 @@ function onTicTacToeSquareClick(){
 class TicTacToeGame{
     playerOneScore;
     playerTwoScore;
+    currTicTacToeRound;
 
     constructor(){
         playerOneScore = 0;
         playerTwoScore = 0;
+        currTicTacToeRound = startNewTicTacToeRound();
+    }
+
+    startNewTicTacToeRound(){
+        currTicTacToeRound = new TicTacToeRound();
     }
 }
