530db1bed1cdf726de2e3c726288d74f0ccc2083
Matthew Ruczak <MatthewRuczak@gmail.com>
2021-04-23 22:46:56 -0500

    Removed duplicate code as it will remain in the Driver Files.

--- js/ticTacToeGame.js
+++ js/ticTacToeGame.js
@@ -1,10 +1,3 @@
-const playersTurnElement = $("#playersTurn"); //Holding reference to the GUI text to show who's turn it is
-const playerOneScoreElement = $('#playerOneScore'); //Holding reference to the GUI text for player 1's score
-const playerTwoScoreElement = $('#playerTwoScore'); //Holding reference to the GUI text for player 2's score
-const notificationOuterBox = $('#notificationOuterBox');
-const notificationMessage = $('#notificationMessage');
-const ticTacToeSquareElements = $(".ticTacToeSquares"); //Getting reference to all tic tac toe squares
-
 export class TicTacToeGame{
     playerOneScore;
     playerTwoScore;
