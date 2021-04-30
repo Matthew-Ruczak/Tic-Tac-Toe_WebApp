c74b63fe8e6b1b2d413aa623b421786e38b8361a
Matthew-Ruczak <matthew@ruczak.ca>
2021-04-30 03:26:03 -0500

    Updated file with the Code from Single Player & Two Player

--- js/game.js
+++ js/game.js
@@ -1,23 +1,83 @@
-export class TicTacToeGame{
-    playerOneScore;
-    playerTwoScore;
-    currTicTacToeRound;
+import { TicTacToe } from "./tic-tac-toe";
 
-    constructor(){
-        playerOneScore = 0;
-        playerTwoScore = 0;
-        currTicTacToeRound = startNewTicTacToeRound();
-    }
+//Setting up variables
+let playerOneScore = 0;
+let playerTwoScore = 0;
+let currTicTacToeRound;
 
-    startNewTicTacToeRound(){
-        currTicTacToeRound = new TicTacToeRound();
-    }
+//Setting up References to HTML Elements
+let playersTurnElement = $("#playersTurn");
+let playerOneScoreElement = $('#playerOneScore');
+let playerTwoScoreElement = $('#playerTwoScore'); 
+let notificationOuterBox = $('#notificationOuterBox');
+let notificationMessage = $('#notificationMessage');
+let allSquareElementRef = $(".ticTacToeSquares");
 
-    onTicTacToeSquareClick(x,y){
-        this.currTicTacToeRound.placeAt(x,y);
-    }
+//Setting up HTML buttons
+$('#restartGameBtn').on('click', restartGame);
+$('#closeNotificationBox').on('click', closeNotificationBox);
 
-    get PlayersTurn(){
-        this.currTicTacToeRound.getPlayersTurn();
+//Making Squares Clickable
+allSquareElementRef.on('click', onTicTacToeSquareClick);
+
+//Starting the Tic Tac Toe Game
+startNewTicTacToeRound();
+
+function startNewTicTacToeRound(){
+    currTicTacToeRound = new TicTacToe();
+    //Clearing all Square HTML Elements
+    allSquareElementRef.html('');
+    updateHTMLGUI();
+}
+
+function restartGame(){
+    //Zeroing Scores
+    playerOneScore = 0;
+    playerTwoScore = 0;
+    currTicTacToeRound = new TicTacToe();
+    //Clearing all Square HTML Elements
+    allSquareElementRef.html('');
+    updateHTMLGUI()
+}
+
+function updateHTMLGUI(){
+    playersTurnElement.html(currTicTacToeRound.currPlayerTurn);
+    playerOneScoreElement.html(playerOneScore);
+    playerTwoScoreElement.html(playerTwoScore);
+}
+
+function onTicTacToeSquareClick(){
+    let result = this.currTicTacToeRound.placeAt($(this).attr('data-pos-x'), $(this).attr('data-pos-y'));
+    if (result){
+        //Updating HTML Square with the Players Symbol
+        $(this).html(currTicTacToeRound.currPlayerTurn === 1 ? 'X' : 'O');
+        
+        //Checking if the game is over
+        if (result === 1){
+            openNotificationBox("Player 1 Win's this Round!");
+            startNewTicTacToeRound();
+        }
+        else if (result === 2){
+            openNotificationBox("Player 2 Win's this Round!");
+            startNewTicTacToeRound();
+        }
+        else if (result === "D"){
+            openNotificationBox("This Round is a Draw!");
+            startNewTicTacToeRound();
+        }
+        updateHTMLGUI();
     }
+    else{
+        openNotificationBox("This square has been played!");
+    }
+}
+
+function openNotificationBox(message){
+    notificationMessage.html(message); //Display the given message in the notification box
+    notificationOuterBox.show();
+}
+
+function closeNotificationBox(){
+    notificationMessage.html(""); //Removing text from notification box
+    notificationOuterBox.hide();
 }
